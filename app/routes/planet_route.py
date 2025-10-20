21f91ac7f37d291910789b7c7dab959e8c397cba
TrangLe <alicelee1512@gmail.com>
2025-10-20 11:32:45 -0700

    Group2 Wave2 Planet

--- app/routes/planet_route.py
+++ app/routes/planet_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planet import planets
 
 
@@ -16,6 +16,27 @@ def get_all_planets():
         })
     return planet_response
 
+@planet_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "mass": planet.mass
+    }
 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
 
 
